cede1f6794365e21bf209e764983b50f39b83938
za-yn <yuan.z@u.nus.edu>
2019-03-20 22:47:23 +0800

    added music, and running it in main.cpp loop. music to be changed later.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "motors.h"
 #include "bluetooth.h"
 #include "queues.h"
+#include "music.h"
 
 #define STACK_SIZE 200
 #define maxMotorDelayCommand portMAX_DELAY
@@ -45,7 +46,7 @@ void setup() {
 }
 
 void loop() {
-
+	playTune();
 	xTaskCreate(xTaskMotor, "TaskMotor", STACK_SIZE, NULL, 3, NULL);
 	vTaskStartScheduler();
 }
